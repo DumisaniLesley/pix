b1791599a13d16c2748c0f061e72c52dbeafd16b
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-03-01 15:01:12 +0200

    Refactor Story component styling

--- components/Home/Story.jsx
+++ components/Home/Story.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Story = ({user, image}) => {
   return (
     <div >
-        <div className='p-[4px] rounded-2xl border-[1px] border-black w-[70px] h-[70px]'>
-          <Image src={image} width={70} height={70} alt='story' className='border-[1px] border-black rounded-2xl object-cover'/>
+        <div className='p-[4px] rounded-full border-[1px] border-black w-[70px] h-[70px]'>
+          <Image src={image} width={70} height={70} alt='story' className='border-[1px] border-black rounded-full object-cover'/>
         </div>
         <p className='text-center text-[12px] mt-1 font-bold'>{user}</p>
     </div>
