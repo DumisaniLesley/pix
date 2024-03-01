a9f17bd7483f31445ee5ff4261c8ba7ff0e9e875
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-03-01 20:41:46 +0200

    Add user profile image to header and search bar

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -1,15 +1,24 @@
+import { currentUser } from '@clerk/nextjs'
+import Image from 'next/image'
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
 
-const Header = () => {
+const Header = async () => {
+
+  const user = await currentUser()
+
   return (
     <header className='fixed top-0 left-0 w-full py-3 shadow-sm bg-white'>
-      <div className='flex justify-between items-center mx-auto w-full space-x-[270px] px-4'>
+      <div className='flex justify-between items-center mx-auto w-full  px-4'>
         <div>
               <h1 className='font-extrabold text-[22px]'>Pix</h1>
           </div>
+      
+          <div className=' flex justify-center'>
+        <input type="text" placeholder='Search...' className='w-[170px] px-2 py-1 rounded-full bg-gray-200 placeholder:text-black placeholder:font-light placeholder:text-sm placeholder:px-1 focus:outline-none focus:cursor-wait'/>
+          </div>
           <div>
-              <FaSearch className='text-xl'/>
+              <Image src={user.imageUrl} width={45} height={45} alt='story' className='rounded-full'/>
           </div>
       </div>
     </header>
