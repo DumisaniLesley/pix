6e0a33ae04ca5f1c16fd4e69202625427d09c8b2
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-03-01 20:49:13 +0200

    Refactor Header component to use next/image and add user avatar

--- components/Shared/Header.jsx
+++ components/Shared/Header.jsx
@@ -1,17 +1,21 @@
 import Link from 'next/link'
-import { IoMdExit } from 'react-icons/io';
+import Image from 'next/image'
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
+import { currentUser } from '@clerk/nextjs';
 
-const Header = () => {
+const Header = async () => {
+
+  const user = await currentUser()
+
   return (
     <div className='px-4 fixed top-0 left-0 w-full shadow-sm bg-white'>
-      <div className='flex justify-between py-3'>
+      <div className='flex justify-between items-center py-3'>
         <Link href='/'>
             <MdOutlineKeyboardBackspace className='text-3xl'/>
           </Link>
           <Link href='/' className='font-extrabold text-[22px]'>Pix</Link>
           <div>
-              <IoMdExit className='text-3xl font-bold'/>
+              <Image src={user.imageUrl} width={45} height={45} alt='story' className='rounded-full'/>
           </div>
       </div>
     </div>
