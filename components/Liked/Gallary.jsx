06725d317923007bb93e0392f4aa50ed945e62a9
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-03-01 13:43:32 +0200

    Add camera and video icons to Liked Gallery

--- components/Liked/Gallary.jsx
+++ components/Liked/Gallary.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
-import React from 'react'
+import { FaCamera  } from "react-icons/fa";
+import { MdVideocam } from "react-icons/md";
 
 const images = [
     {
@@ -32,6 +33,13 @@ const images = [
 const Gallary = () => {
   return (
     <div className='p-5 md:p-10'>
+    <div className='flex justify-between'>
+        <h4 className='font-bold'>Liked Posts</h4>
+            <div className='flex gap-4'>
+                <FaCamera className='text-2xl'/>
+                <MdVideocam className='text-3xl'/>
+            </div>
+        </div>
         <div className='columns-2 gap-5 lg:gap-8 lg:columns-3 xl:columns-4 [&>Image:not(:first-child)]:mt-5 lg:[&>Image:not(:first-child)]:mt-8'>
             {images.map(image => {
             return <Image key={image.id} src={image.src} width={200} height={200} alt={image.alt} className='rounded-md mb-4'/> 
