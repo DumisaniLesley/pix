7d29f98e4836b77b8f0575668777d18ae99ecbe4
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-03-01 15:34:00 +0200

    Refactor Header component styling

--- components/Shared/Header.jsx
+++ components/Shared/Header.jsx
@@ -4,8 +4,8 @@ import { MdOutlineKeyboardBackspace } from "react-icons/md";
 
 const Header = () => {
   return (
-    <div className='px-4 fixed top-0 left-0 w-full py-3 shadow-sm bg-white'>
-      <div className='flex justify-between'>
+    <div className='px-4 fixed top-0 left-0 w-full shadow-sm bg-white'>
+      <div className='flex justify-between py-3'>
         <Link href='/'>
             <MdOutlineKeyboardBackspace className='text-3xl'/>
           </Link>
